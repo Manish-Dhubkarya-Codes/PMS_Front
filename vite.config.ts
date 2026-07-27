e7d4b2351395fca0b8ae0554d2a25f29add31ae0
Manish Dhubkarya <manishdhubkaryacloud@gmail.com>
2026-07-27 13:36:13 +0530

    Refactor Vite configuration: Replace hardcoded API URL with dynamic environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -2,34 +2,38 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 
+
+ const url = (import.meta as any).env.VITE_API_URL || 'http://localhost:3000';
+
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+
   
   server: {
     port: 5173,
     proxy: {
       // Proxy Socket.io (critical for WS upgrades)
       '/socket.io': {
-        target: 'http://localhost:3000',
+        target: url,
         ws: true,
         changeOrigin: true,
         secure: false,
       },
       // Optional: Proxy API routes too
       '/api': {
-        target: 'http://localhost:3000',
+        target: url,
         changeOrigin: true,
         secure: false,
       },
      
       '/vapid-public-key': {
-        target: 'http://localhost:3000',
+        target: url,
         changeOrigin: true,
         secure: false,
       },
       // Add if needed for other routes (e.g., /images)
       '/images': {
-        target: 'http://localhost:3000',
+        target: url,
         changeOrigin: true,
         secure: false,
       },
